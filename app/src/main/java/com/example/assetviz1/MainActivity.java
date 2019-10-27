bb8947226df90354befb22ce0daf0791b9c75abc
anthonypeters <apeters10@luc.edu>
2019-10-26 19:11:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/assetviz1/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.assetviz1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    //Variables
+    int cash;
+    int cryptos;
+    int stocks;
+    int bonds;
+    int comMoney;
+    int realEstate;
+    int land;
+    int vehicles;
+
+    //Button
+    Button button;
+
+    //Input boxes
+    EditText cashInput;
+    EditText cryptosInput;
+    EditText stocksInput;
+    EditText bondsInput;
+    EditText comMoneyInput;
+    EditText realEstateInput;
+    EditText landInput;
+    EditText vehiclesInput;
+
+    public void onCreate (Bundle information){
+        super.onCreate(information);
+        setContentView(R.layout.content_main);
+
+        cashInput = (EditText) findViewById(R.id.cashInput);
+        cryptosInput = (EditText) findViewById(R.id.cryptosInput);
+        stocksInput = (EditText) findViewById(R.id.stocksInput);
+        bondsInput = (EditText) findViewById(R.id.bondsInput);
+        comMoneyInput = (EditText) findViewById(R.id.comMoneyInput);
+        realEstateInput = (EditText) findViewById(R.id.realEstateInput);
+        landInput = (EditText) findViewById(R.id.landInput);
+        vehiclesInput = (EditText) findViewById(R.id.vehiclesInput);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                cash = Integer.valueOf(cashInput.getText().toString());
+                cryptos = Integer.valueOf(cryptosInput.getText().toString());
+                stocks = Integer.valueOf(stocksInput.getText().toString());
+                bonds = Integer.valueOf(bondsInput.getText().toString());
+                comMoney = Integer.valueOf(comMoneyInput.getText().toString());
+                realEstate = Integer.valueOf(realEstateInput.getText().toString());
+                land = Integer.valueOf(landInput.getText().toString());
+                vehicles = Integer.valueOf(vehiclesInput.getText().toString());
+            }
+        });
+    }
+
+
+
+}
